cb912a8b92e3980bef5f52c05d86fb0269cef66a
Konrad Wojda <konrad2001@outlook.com>
2023-04-03 19:50:45 +0900

    Registered models to django admin site

--- szallitas/transportation/admin.py
+++ szallitas/transportation/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.Agency)
+admin.site.register(models.Calendar)
+admin.site.register(models.CalendarException)
+admin.site.register(models.Line)
+admin.site.register(models.Stop)
+admin.site.register(models.Pattern)
+admin.site.register(models.PatternStop)
+admin.site.register(models.Trip)
