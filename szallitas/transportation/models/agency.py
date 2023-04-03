57c69c84f55368be7198e90d23ee6cbdb2cf31f4
Konrad Wojda <konrad2001@outlook.com>
2023-04-03 16:36:11 +0900

    Added agency model

---
+++ szallitas/transportation/models/agency.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Agency(models.Model):
+    name = models.CharField(max_length=64)
+    website = models.CharField(max_length=128)
+    timezone = models.CharField(max_length=64, null=True, blank=True)
+    telephone = models.CharField(max_length=32, null=True, blank=True)
