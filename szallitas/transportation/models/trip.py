07db969dfcdb760df37fc40e7b89b4031fe61c5f
Konrad Wojda <konrad2001@outlook.com>
2023-04-03 17:43:18 +0900

    Added calendar field in trip model

--- szallitas/transportation/models/trip.py
+++ szallitas/transportation/models/trip.py
@@ -1,9 +1,11 @@
 from django.db import models
 from .stop import WheelchairAccessibility
 from .pattern import Pattern
+from .calendar import Calendar
 
 
 class Trip(models.Model):
     wheelchair_accessible = models.IntegerField(choices=WheelchairAccessibility.choices)
     departure = models.IntegerField(max_length=6)
     pattern = models.ForeignKey(Pattern, on_delete=models.CASCADE)
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
