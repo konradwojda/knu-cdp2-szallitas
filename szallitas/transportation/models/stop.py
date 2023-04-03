27aec0090010135f2aa61b22a6d18a2c47a27bc4
Konrad Wojda <konrad2001@outlook.com>
2023-04-03 19:25:56 +0900

    Added decimal_places and max_digits to lat and lon

--- szallitas/transportation/models/stop.py
+++ szallitas/transportation/models/stop.py
@@ -10,6 +10,6 @@ class WheelchairAccessibility(models.IntegerChoices):
 class Stop(models.Model):
     name = models.CharField(max_length=64)
     code = models.CharField(max_length=16, null=True, blank=True)
-    lat = models.DecimalField()
-    lon = models.DecimalField()
+    lat = models.DecimalField(decimal_places=6, max_digits=9)
+    lon = models.DecimalField(decimal_places=6, max_digits=9)
     wheelchair_accessible = models.IntegerField(choices=WheelchairAccessibility.choices)
