2fa23f6308115a11c68e37ef3fa9ed2a89faf57d
Konrad Wojda <konrad2001@outlook.com>
2023-04-03 17:38:27 +0900

    Black reformat

--- szallitas/transportation/models/stop.py
+++ szallitas/transportation/models/stop.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 class WheelchairAccessibility(models.IntegerChoices):
     NO_INFO = 0
     ACCESSIBLE = 1
     NOT_ACCESSIBLE = 2
 
-class Stop(models.Model):
 
+class Stop(models.Model):
     name = models.CharField(max_length=64)
     code = models.CharField(max_length=16, null=True, blank=True)
     lat = models.DecimalField()
