052d9794c2f0ab3e19e1181d6051799fc6d19aaa
Konrad Wojda <konrad2001@outlook.com>
2023-04-03 20:00:46 +0900

    Added prular verbose name for agency

--- szallitas/transportation/models/agency.py
+++ szallitas/transportation/models/agency.py
@@ -6,3 +6,6 @@ class Agency(models.Model):
     website = models.CharField(max_length=128)
     timezone = models.CharField(max_length=64, null=True, blank=True)
     telephone = models.CharField(max_length=32, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Agencies"
