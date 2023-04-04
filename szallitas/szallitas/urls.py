8098b0ca9bfe4cd9194f819516f57edc16cb1aca
Konrad Wojda <konrad2001@outlook.com>
2023-04-04 21:37:49 +0900

    Redirecting to main view

--- szallitas/szallitas/urls.py
+++ szallitas/szallitas/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/transportation/"), name="home"),
     path("admin/", admin.site.urls),
     path("transportation/", include("transportation.urls")),
 ]
