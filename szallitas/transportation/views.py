d9b1133511391f498b4195b820e69d98b631a097
Getsug <riderlua@protonmail.com>
2023-04-28 18:47:50 +0900

    reordered imports

--- szallitas/transportation/views.py
+++ szallitas/transportation/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse, JsonResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
 
 
 from .models.line import Line
