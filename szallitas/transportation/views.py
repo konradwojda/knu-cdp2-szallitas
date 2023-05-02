80dd52d6535ff9b6df2b769bcd774f1196c2ef22
Konrad Wojda <konrad2001@outlook.com>
2023-05-02 17:37:09 +0900

    Passing only lines that stop at particular stop

--- szallitas/transportation/views.py
+++ szallitas/transportation/views.py
@@ -18,7 +18,7 @@ def line(request: HttpRequest, line_id: int) -> HttpResponse:
 def stop(request: HttpRequest, stop_id: int) -> HttpResponse:
     stop = get_object_or_404(Stop, id=stop_id)
 
-    context = {"lines": Line.objects.all(), "stop": stop}
+    context = {"lines": Line.objects.filter(pattern__stops__id=1).distinct(), "stop": stop}
     return render(request, "transportation/stop.html", context)
 
 
