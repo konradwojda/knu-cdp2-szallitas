9426d3bc5fd48e374dd6cbe0c487e4d3b34341fd
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-05-05 18:43:39 +0900

    Load fixture WKD database in GTFSExport tests

--- szallitas/transportation/gtfs_tools/test_gtfs_export.py
+++ szallitas/transportation/gtfs_tools/test_gtfs_export.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 
+from ..management.commands.load_sample_data import load_wkd_fixture_database
+from ..models import Agency, Line
+
 
 class GTFSExportTestCase(TestCase):
+    def setUp(self) -> None:
+        load_wkd_fixture_database()
+
     def test(self) -> None:
-        self.assertEqual(1, 1)
+        self.assertEqual(Agency.objects.count(), 1)
+        self.assertEqual(Line.objects.count(), 3)
