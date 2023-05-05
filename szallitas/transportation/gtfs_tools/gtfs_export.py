c932eb3b0d4901646bf85f0c7161f03b71bed60c
Mikołaj Kuranowski <mkuranowski@gmail.com>
2023-05-05 18:56:31 +0900

    GTFSExporter: agencies

--- szallitas/transportation/gtfs_tools/gtfs_export.py
+++ szallitas/transportation/gtfs_tools/gtfs_export.py
@@ -1,2 +1,33 @@
+import csv
+import logging
+from typing import IO
+
+from ..models import Agency
+
+logger = logging.getLogger(__name__)
+
+
 class GTFSExporter:
-    pass
+    @staticmethod
+    def export_agencies(to: IO[str]) -> None:
+        w = csv.writer(to)
+        w.writerow(("agency_id", "agency_name", "agency_url", "agency_timezone", "agency_phone"))
+
+        for agency in Agency.objects.all():
+            # Warn if required GTFS fields are missing
+            if not agency.timezone:
+                logger.warn(
+                    "Agency %s (ID %d) has no timezone - using UTC",
+                    agency.name,
+                    agency.id,
+                )
+
+            w.writerow(
+                (
+                    agency.id,
+                    agency.name,
+                    agency.website,
+                    agency.timezone or "UTC",
+                    agency.telephone or "",
+                )
+            )
--- szallitas/transportation/gtfs_tools/test_gtfs_export.py
+++ szallitas/transportation/gtfs_tools/test_gtfs_export.py
@@ -1,13 +1,22 @@
+from io import StringIO
+
 from django.test import TestCase
 
 from ..management.commands.load_sample_data import load_wkd_fixture_database
-from ..models import Agency, Line
+from .gtfs_export import GTFSExporter
 
 
 class GTFSExportTestCase(TestCase):
     def setUp(self) -> None:
         load_wkd_fixture_database()
 
-    def test(self) -> None:
-        self.assertEqual(Agency.objects.count(), 1)
-        self.assertEqual(Line.objects.count(), 3)
+    def test_export_agencies(self) -> None:
+        f = StringIO()
+        GTFSExporter.export_agencies(f)
+        self.assertMultiLineEqual(
+            f.getvalue(),
+            (
+                "agency_id,agency_name,agency_url,agency_timezone,agency_phone\r\n"
+                "1,WKD,https://wkd.com.pl,Europe/Warsaw,\r\n"
+            ),
+        )
