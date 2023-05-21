91e088f4ebf45e76d4f9bdb541163d06f9c66004
kpsuty <01158715@pw.edu.pl>
2023-05-22 00:16:47 +0900

    Initial version of generate_tabular_timetable()

--- szallitas/transportation/timetable/tabular.py
+++ szallitas/transportation/timetable/tabular.py
@@ -1,24 +1,43 @@
+from datetime import timedelta, time
+from typing import List, Tuple
 from ..models import PatternStop
 
 Hour = str
-Minutes = list[str]
+Minutes = List[str]
 CalendarName = str
-DepartureBoard = list[tuple[Hour, Minutes]]
-DepartureBoardByCalendar = list[tuple[CalendarName, DepartureBoard]]
-
+DepartureBoard = List[Tuple[Hour, Minutes]]
+DepartureBoardByCalendar = List[Tuple[CalendarName, DepartureBoard]]
 
 def generate_tabular_timetable(_pattern_stop: PatternStop) -> DepartureBoardByCalendar:
-    return [
-        (
-            "Weekday",
-            [(f"{h:0>2}", ["00", "20", "40"]) for h in range(6, 21)],
-        ),
-        (
-            "Saturday",
-            [(f"{h:0>2}", ["15", "45"]) for h in range(7, 20)],
-        ),
-        (
-            "Sunday",
-            [(f"{h:0>2}", ["30"]) for h in range(8, 19)],
-        ),
-    ]
+    timetable: DepartureBoardByCalendar = []
+
+    trips = _pattern_stop.pattern.trip_set.all()
+
+    for trip in trips:
+        departure_time = trip.departure + _pattern_stop.travel_time
+        departure_time += timedelta(days=1)
+        departure_time = departure_time.total_seconds() % (24 * 3600)
+        departure_time = time(hour=int(departure_time // 3600), minute=int((departure_time // 60) % 60))
+
+        calendar_name = trip.calendar.name
+
+        departure_board: DepartureBoard = []
+        for entry in timetable:
+            if entry[0] == calendar_name:
+                departure_board = entry[1]
+                break
+
+        if not departure_board:
+            timetable.append((calendar_name, departure_board))
+
+        hour = str(departure_time.hour).zfill(2)
+        minutes = str(departure_time.minute).zfill(2)
+
+        for entry in departure_board:
+            if entry[0] == hour:
+                entry[1].append(minutes)
+                break
+        else:
+            departure_board.append((hour, [minutes]))
+
+    return timetable
