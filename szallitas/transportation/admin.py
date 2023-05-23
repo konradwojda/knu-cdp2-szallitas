4f202115cfb2ec9197045ecd934cb7ee7baffd8a
Getsug <riderlua@protonmail.com>
2023-05-23 12:39:16 +0900

    added import to admin index page

--- szallitas/transportation/admin.py
+++ szallitas/transportation/admin.py
@@ -13,6 +13,8 @@ class CsvImportForm(forms.Form):
     zip_import = forms.FileField()
 
 class TransportAdminSite(admin.AdminSite):
+    index_template = "admin/custom_index.html"
+
     def get_urls(self):
         urls = super().get_urls()
         new_urls = [
@@ -41,19 +43,9 @@ class TransportAdminSite(admin.AdminSite):
             gtfs_loader = GTFSLoader()
             gtfs_loader.from_zip(zip_file)
 
-            # file_data = zip_file.read().decode("utf-8")
-            # csv_data = file_data.split("\n")
-
-            # for x in csv_data:
-            #     fields = x.split(",")
-            #     models.Import.objects.update_or_create(
-            #         filename=fields[0],
-            #     )
-
-            # self.updateModel(csv_data, request.path_info)
             models.Import.objects.update_or_create(filename=zip_name)
 
-            self.message_user(request, "Your zip file has been uploaded")
+            messages.success(request, "Your zip file has been uploaded")
             return redirect("..")
 
         form = CsvImportForm()
@@ -78,47 +70,4 @@ admin_site.register(models.Pattern)
 admin_site.register(models.PatternStop)
 admin_site.register(models.Stop)
 admin_site.register(models.Trip)
-
-
-
-class ImportAdmin(admin.ModelAdmin):
-    list_display = ("filename", "created")
-
-    def get_urls(self):
-        urls = super().get_urls()
-        new_urls = [
-            path("upload-zip/", self.upload_zip),
-        ]
-        return new_urls + urls
-
-    def upload_zip(self, request):
-        if request.method == "POST":
-            zip_file = request.FILES["zip_import"]
-            zip_name = request.FILES["zip_import"].name
-
-            if not zip_file.name.endswith(".zip"):
-                messages.warning(request, "The wrong file type was uploaded.")
-                return HttpResponseRedirect(request.path_info)
-            gtfs_loader = GTFSLoader()
-            gtfs_loader.from_zip(zip_file)
-
-            # file_data = zip_file.read().decode("utf-8")
-            # csv_data = file_data.split("\n")
-
-            # for x in csv_data:
-            #     fields = x.split(",")
-            #     models.Import.objects.update_or_create(
-            #         filename=fields[0],
-            #     )
-
-            # self.updateModel(csv_data, request.path_info)
-            models.Import.objects.update_or_create(filename=zip_name)
-
-            self.message_user(request, "Your zip file has been uploaded")
-            return redirect("..")
-
-        form = CsvImportForm()
-        data = {"form": form}
-
-        return render(request, "admin/csv_upload.html", data)
     
--- szallitas/transportation/models/__init__.py
+++ szallitas/transportation/models/__init__.py
@@ -1,6 +1,5 @@
 from .agency import Agency
 from .calendar import Calendar, CalendarException
-from .import_export import Import
 from .line import Line
 from .pattern import Pattern, PatternStop
 from .stop import Stop, WheelchairAccessibility
